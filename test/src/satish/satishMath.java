8e66fddd64fbf328aac241ab71bb62b03a38dfe6
satishcj <satish.cj@vit.ac.in>
2020-03-11 17:37:13 +0530

    addition and subtraction files

---
+++ test/src/satish/satishMath.java
@@ -0,0 +1,17 @@
+package satish;
+
+public class satishMath {
+	
+	//to return the result of addition of two numbers
+	public int sum(int x,int y)
+	{
+		return x+y;
+	}
+	
+	//to return the result of subtraction of two numbers
+	public int subtract(int x,int y)
+	{
+		return x-y;
+	}
+
+}
---
+++ test/src/test/classDemo.java
@@ -0,0 +1,10 @@
+package test;
+import satish.satishMath;
+public class classDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		satishMath k = new satishMath();
+		System.out.println(k.subtract(3, 2));
+		System.out.println(k.sum(3, 2));
+	}
+}
